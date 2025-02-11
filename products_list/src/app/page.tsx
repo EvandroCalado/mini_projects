d31f853a88bfab8f5f774473589edeb15bf20189
Evandro Calado <evandrocalado07@gmail.com>
2025-02-11 09:33:16 -0300

    feat: initialize Next.js project with TypeScript, Tailwind CSS, and essential configurations

---
+++ products_list/src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main className='mx-auto flex h-screen max-w-6xl flex-col items-center justify-center gap-10 p-5'>
+      <h1>HomePage</h1>
+    </main>
+  );
+};
+
+export default HomePage;
