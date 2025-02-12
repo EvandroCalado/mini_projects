5493c95c3760f8810c9bb4ee8a8de097610c7b27
Evandro Calado <evandrocalado07@gmail.com>
2025-02-11 21:08:00 -0300

    feat: fetch and display products using ProductCard component

--- products_list/src/app/page.tsx
+++ products_list/src/app/page.tsx
@@ -1,32 +1,21 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui';
+import { notFound } from 'next/navigation';
 
-const HomePage = () => {
+import { getProductsAction } from '@/actions/get-products.action';
+import { ProductCard } from '@/components/products';
+
+const HomePage = async () => {
+  const products = await getProductsAction();
+
+  if (!products) notFound();
+
   return (
-    <main className='mx-auto flex h-screen max-w-6xl flex-col items-center justify-center gap-10 p-5'>
+    <main className='mx-auto flex max-w-6xl flex-col items-center justify-center gap-10 p-5'>
       <h1>Products List</h1>
 
       <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-        <Card>
-          <CardHeader>
-            <CardTitle>Title</CardTitle>
-            <CardDescription>Description</CardDescription>
-
-            <CardContent>
-              <p>Card content</p>
-            </CardContent>
-
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </CardHeader>
-        </Card>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
     </main>
   );
