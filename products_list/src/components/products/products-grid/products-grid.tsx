246742f7273f1c841c86c728ba96749b571ec97f
Evandro Calado <evandrocalado07@gmail.com>
2025-02-13 23:19:57 -0300

    feat: add ProductsGrid component for improved product display

---
+++ products_list/src/components/products/products-grid/products-grid.tsx
@@ -0,0 +1,19 @@
+import { Product } from '@/types';
+import { ProductsCard } from '../products-card';
+import { ProductsEmpty } from '../products-empty';
+
+type ProductsGridProps = {
+  products: Product[];
+};
+
+export const ProductsGrid = ({ products }: ProductsGridProps) => {
+  return (
+    <div className='grid h-max w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
+      {products.length < 1 && <ProductsEmpty />}
+
+      {products.map((product) => (
+        <ProductsCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
