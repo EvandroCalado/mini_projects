9b223959de0e4aab479cca23b058e5f63b904835
Evandro Calado <evandrocalado07@gmail.com>
2025-02-13 23:20:03 -0300

    fix: add margin to ProductsEmpty component for improved spacing

--- products_list/src/components/products/products-empty/products-empty.tsx
+++ products_list/src/components/products/products-empty/products-empty.tsx
@@ -2,7 +2,7 @@ import { Frown } from 'lucide-react';
 
 export const ProductsEmpty = () => {
   return (
-    <div className='flex h-full w-full flex-col items-center justify-center gap-4 text-muted-foreground md:col-span-2 lg:col-span-3'>
+    <div className='my-20 flex h-full w-full flex-col items-center justify-center gap-4 text-muted-foreground md:col-span-2 lg:col-span-3'>
       <Frown size={40} />
       <h1 className='text-xl font-semibold'>Products not found</h1>
     </div>
